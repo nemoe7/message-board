3a8783c512a9ced6e38f7d6b3930e36cd4e18876
Alceaus <danielmarasigan2000@yahoo.com>
2023-04-05 22:00:52 +0800

    Python Message Board
    it's working, only tried most of the command test in the demo kit.

---
+++ PythonMessageBoard/Client.py
@@ -0,0 +1,189 @@
+def join(userCommand):  
+    try:
+        inputCommand = str.split(userCommand," ")
+        jsonFormat =  { "command":inputCommand[0]}
+
+        # converting to JSON
+        intoJSON = json.dumps(jsonFormat)
+        bytesSend = str.encode(intoJSON)
+        serverAP = (inputCommand[1], int(inputCommand[2])) # Server Address Port
+
+        print(serverAP)
+
+        return bytesSend, serverAP
+    except:
+        print("Error: Command parameters do not match or is not allowed.")
+
+def leave(userCommand):
+    jsonFormat =  { "command":userCommand}
+
+    # converting to JSON
+    intoJSON = json.dumps(jsonFormat)
+    bytesSend = str.encode(intoJSON)
+
+    return bytesSend
+
+def register(userCommand):
+    inputCommand = str.split(userCommand," ")
+    jsonFormat =  { "command":inputCommand[0], "handle":inputCommand[1]}
+
+    # converting to JSON
+    intoJSON = json.dumps(jsonFormat)
+    bytesSend = str.encode(intoJSON)
+
+    return bytesSend
+
+def msg(userCommand):
+    inputCommand = str.split(userCommand," ",2)
+    print(inputCommand)
+    jsonFormat =  { "command":inputCommand[0], "handle":inputCommand[1], "message":inputCommand[2]}
+
+    # converting to JSON
+    intoJSON = json.dumps(jsonFormat)
+    bytesSend = str.encode(intoJSON)
+
+    return bytesSend
+
+def all(userCommand):
+    inputCommand = str.split(userCommand," ",1)
+    jsonFormat =  { "command":inputCommand[0], "message":inputCommand[1]}
+
+    # converting to JSON
+    intoJSON = json.dumps(jsonFormat)
+    bytesSend = str.encode(intoJSON)
+
+    return bytesSend
+
+def printServer(bytesAddressPair):
+    convertMsg = str(bytesAddressPair[0], encoding)
+    jsonMsg = json.loads(convertMsg)
+    serverMsg = "\nMessage from Server:{}".format(jsonMsg["message"])
+    print(serverMsg)
+
+# Listen for incoming datagrams
+def sender():
+    global serverAddressPort
+    global joined
+    while(True):
+        userCommand = input("Enter command: ")
+
+        if "/join" in userCommand:
+            try:
+                lock.acquire() # Acquire lock before modifying joined variable
+                joined = True
+                bytesSend, serverAddressPort = join(userCommand)
+
+                # Sent to the server using UDP Socket
+                UDPClientSocket.sendto(bytesSend, serverAddressPort)
+
+            except:
+                print("Error: Connection to the Message Board Server has failed!")
+                print("Please check IP Address and Port Number")
+            finally:
+                lock.release() # Release lock after modifying joined variable
+
+        elif "/leave" in userCommand:
+            lock.acquire() # Acquire lock before modifying joined variable
+            if joined == True:
+                bytesSend = leave(userCommand)
+
+                # Sent to the server using UDP Socket
+                UDPClientSocket.sendto(bytesSend, serverAddressPort)
+                joined = False
+                break
+            else:
+                print("Error: Disconnection failed. Please connect to the server first.")
+            
+            lock.release() # Release lock after modifying joined variable
+
+        elif "/register" in userCommand:
+            bytesSend = register(userCommand)
+            # Sent to the server using UDP Socket
+            UDPClientSocket.sendto(bytesSend, serverAddressPort)
+
+        elif "/msg" in userCommand:
+            bytesSend = msg(userCommand)
+            # Sent to the server using UDP Socket
+            UDPClientSocket.sendto(bytesSend, serverAddressPort)
+
+        elif "/all" in userCommand: 
+            bytesSend = all(userCommand)
+            # Sent to the server using UDP Socket
+            UDPClientSocket.sendto(bytesSend, serverAddressPort)
+
+        elif "/?" in userCommand:
+            print(" ")
+            print("------ SYNTAX COMMANDS ------")
+            print(" ")
+            print("Connect to Server App: /join <server_ip_add> <port>")
+            print("Disconnect from Server App: /leave")
+            print("Register unique hand;e: /register <handle>")
+            print("Send message to all: /all <message>")
+            print("Send direct messages to a single handle: /msg <handle> <message>")
+            print("Syntax commands references: /?")
+            print("----------------------------")
+            print(" ")
+            
+        else:
+            print("Error: Command not found.")
+
+        time.sleep(1)
+
+def receiver():
+
+    while(True):
+        bytesAddressPair = UDPClientSocket.recvfrom(bufferSize) # Receive from Server a tuple (bytes, address)
+        printServer(bytesAddressPair)
+
+import json
+import socket
+import threading
+import time
+
+encoding = 'utf-8'
+bufferSize = 1024          
+global serverAddressPort
+global joined
+joined = False
+
+# Creating a UDP socket at client side
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+lock = threading.Lock()
+
+while (joined==False):
+    userStartCommand = input("Enter command: ")
+    if "/join" in userStartCommand:
+        try:
+            bytesSend, serverAddressPort = join(userStartCommand)
+
+            # Sent to the server using UDP Socket
+            print(serverAddressPort)
+            UDPClientSocket.sendto(bytesSend, serverAddressPort)
+            joined = True
+
+        except:
+            print("Error: Connection to the Message Board Server has failed!")
+            print("Please check IP Address and Port Number")
+    elif "/?" in userStartCommand:
+            print(" ")
+            print("------ SYNTAX COMMANDS ------")
+            print(" ")
+            print("Connect to Server App: /join <server_ip_add> <port>")
+            print("Disconnect from Server App: /leave")
+            print("Register unique hand;e: /register <handle>")
+            print("Send message to all: /all <message>")
+            print("Send direct messages to a single handle: /msg <handle> <message>")
+            print("Syntax commands references: /?")
+            print("----------------------------")
+            print(" ")
+    else:
+        print("Error: Command not found.")
+
+receive = threading.Thread(target=receiver)
+send = threading.Thread(target=sender)
+
+if (joined==True):
+    print("Thread started")
+    receive.start()
+    send.start()
---
+++ PythonMessageBoard/Server.py
@@ -0,0 +1,88 @@
+def msgToClient(msgFromServer):
+    jsonFormat =  {"message":msgFromServer}
+    intoJSON = json.dumps(jsonFormat) # converting to JSON
+    bytesSend = str.encode(intoJSON)
+
+    return bytesSend
+
+import socket
+import json
+
+encoding = 'utf-8'
+localIP     = "127.0.0.1"
+localPort   = 12345
+bufferSize  = 1024
+handles = []
+global destAddr
+
+msgFromServer = "Hello UDP Client"
+jsonFormat =  {"message":msgFromServer}
+intoJSON = json.dumps(jsonFormat)
+bytesToSend = str.encode(intoJSON)
+
+# Create a datagram socket
+UDPserver = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+UDPserver.bind((localIP, localPort)) # Bind to address and ip
+
+print("UDP server up and listening")
+
+# Listen for incoming datagrams
+while(True):
+    bytesAddressPair = UDPserver.recvfrom(bufferSize) # Receive from Server a tuple (bytes, address)
+    address = bytesAddressPair[1]
+    convertMsg = str(bytesAddressPair[0], encoding) # Converting message from bytes to str
+    jsonMsg = json.loads(convertMsg) # Parse str to json
+
+    if jsonMsg["command"] == "/join":
+        bytesToSend = msgToClient("Connection Successful! Welcome!")
+        UDPserver.sendto(bytesToSend, address) # Sending a reply to client
+
+    elif jsonMsg["command"] == "/leave":
+        bytesToSend = msgToClient("Connection closed. Thank you!")
+        UDPserver.sendto(bytesToSend, address) # Sending a reply to client
+        print("A connection closed.")
+
+    elif jsonMsg["command"] == "/register":
+        if (len(handles) == 0):
+            handles.append({'handle' : jsonMsg["handle"], 'addr' : address})
+            msgFromServer = "Welcome " + jsonMsg["handle"] + "!"
+            bytesToSend = msgToClient(msgFromServer)
+            print(handles)
+        else:
+            if any(d['handle'] == jsonMsg["handle"] for d in handles):
+                bytesToSend  = msgToClient("Error: Registration failed. Handle or alias already exists.")
+            else:
+                handles.append({'handle' : jsonMsg["handle"], 'addr' : address})
+                msgFromServer = "Welcome " + jsonMsg["handle"] + "!"
+                bytesToSend = msgToClient(msgFromServer)
+                print(handles)
+            
+        UDPserver.sendto(bytesToSend, address) # Sending a reply to client
+
+    elif jsonMsg["command"] == "/msg":
+        if any(d['handle'] == jsonMsg["handle"] for d in handles):
+            destList = next(x for x in handles if x["handle"] == jsonMsg["handle"])
+            destAddr=destList['addr']
+            srcList = next(x for x in handles if x["addr"] == address)
+            srcHandle = srcList['handle']
+
+            msgSrcClient = jsonMsg["message"]
+            bytesSendDest = msgToClient("\n[From " + srcHandle + "]: " + msgSrcClient + "\nEnter command: ")
+            bytesSendSource = msgToClient("\n[To " + jsonMsg["handle"] + "]: " + msgSrcClient)
+
+            UDPserver.sendto(bytesSendDest, destAddr) # Sending a reply to destination client
+            UDPserver.sendto(bytesSendSource, address) # Sending a reply to source client
+        else:
+            bytesToSend = msgToClient("Error: Handle or alias not found.")
+            UDPserver.sendto(bytesToSend, address)
+
+    elif jsonMsg["command"] == "/all":
+        srcList = next(x for x in handles if x["addr"] == address)
+        srcHandle = srcList['handle']
+        msgSrcClient = jsonMsg["message"]
+
+        bytesToSend = msgToClient("\n" + srcHandle + ": " + msgSrcClient + "\nEnter command: ")
+
+        for x in handles:
+            destAddr = x["addr"] 
+            UDPserver.sendto(bytesToSend, destAddr) # Sending a reply to destination clients
